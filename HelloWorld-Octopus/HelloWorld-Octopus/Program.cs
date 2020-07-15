0814e7149cd1fbbe6b7e912622d94ad01acddc6a
youbekonda <68332458+youbekonda@users.noreply.github.com>
2020-07-15 18:34:03 +0530

    Add files via upload

---
+++ HelloWorld-Octopus/HelloWorld-Octopus/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_Octopus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is C#  Vs octopus deployment - @Uday");
+        }
+    }
+}
